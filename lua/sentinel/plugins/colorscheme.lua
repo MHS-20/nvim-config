4a6da28a7537c18fe9699cf78e36f614a24cdc96
Sentinel <muhamad.salman2001@gmail.com>
2025-05-09 21:06:57 +0200

    added lsp support

--- lua/sentinel/plugins/colorscheme.lua
+++ lua/sentinel/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
 
--- return {
---  "ellisonleao/gruvbox.nvim",
---  priority = 1000,
---  config = function()
---    require("gruvbox").setup({
-      -- custom options
---    })
---   vim.cmd("colorscheme gruvbox")
--- end
---}  
-
 return {
-  "folke/tokyonight.nvim",
+  "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
-    require("tokyonight").setup({
+    require("gruvbox").setup({
       -- custom options
-      style = "night"
     })
-    vim.cmd("colorscheme tokyonight")
+   vim.cmd("colorscheme gruvbox")
   end
-} 
+}  
+
+-- return {
+--  "folke/tokyonight.nvim",
+--  priority = 1000,
+--  config = function()
+--    require("tokyonight").setup({
+      -- custom options
+--      style = "night"
+--    })
+--    vim.cmd("colorscheme tokyonight")
+--  end
+--} 
